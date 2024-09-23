64d85aaf5d247b2ab27d282eeccc6555d18814cb
Juan David Cutiva <69434726+Juan-Cutiva@users.noreply.github.com>
2024-09-23 16:37:02 -0500

    "Added @tailwindcss/forms, @tailwindcss/aspect-ratio, and @tailwindcss/typography dependencies and configured them in tailwind.config.mjs; updated settings.json and package.json files"

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,18 +2,42 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		aspectRatio: {
+			auto: 'auto',
+			square: '1 / 1',
+			video: '16 / 9',
+			1: '1',
+			2: '2',
+			3: '3',
+			4: '4',
+			5: '5',
+			6: '6',
+			7: '7',
+			8: '8',
+			9: '9',
+			10: '10',
+			11: '11',
+			12: '12',
+			13: '13',
+			14: '14',
+			15: '15',
+			16: '16'
+		},
 		extend: {
 			screens: {
 				xs: '430px'
 			},
-			colors:{
-				'Green': '#8FA206',
-				'Blue': '#61AEC9',
-				'Pink': '#CC2D4A',
+			colors: {
+				Green: '#8FA206',
+				Blue: '#61AEC9',
+				Pink: '#CC2D4A'
 			}
 		}
 	},
 	plugins: [
+		require('@tailwindcss/aspect-ratio'),
+		require('@tailwindcss/forms'),
+		require('@tailwindcss/typography'),
 		function ({ addComponents, theme }) {
 			addComponents({
 				'.btn': {
