dbf682b3a2e0e32e7ae8e5fea76c3a988572312a
Juan David Cutiva <69434726+Juan-Cutiva@users.noreply.github.com>
2024-09-26 12:57:37 -0500

    [FINISH] Theme dynamic

--- src/scripts/inputTheme.js
+++ src/scripts/inputTheme.js
@@ -1,27 +1,27 @@
 const themeSwitch = document.getElementById('theme-switch');
 const checkbox = document.getElementById('theme-switch__checkbox');
 
-themeSwitch.addEventListener('change', prueba);
-
-
 document.addEventListener('DOMContentLoaded', () => {
 	const themeSwitch = document.querySelector('#theme-switch__checkbox');
-	const root = document.documentElement
+	const root = document.documentElement;
 
-	themeSwitch.addEventListener('click', toggleTheme)
+	themeSwitch.addEventListener('click', toggleTheme);
 
-	function toggleTheme(){
-		const setTheme = themeSwitch.checked ? 'dark':'light'
+	function toggleTheme() {
+		const setTheme = themeSwitch.checked ? 'dark' : 'light';
 
-		root.setAttribute('class', setTheme)
+		root.setAttribute('class', setTheme);
 	}
+});
+document.addEventListener('DOMContentLoaded', () => {
+	const themeSwitch = document.querySelector('#checkboxMoon');
+	const root = document.documentElement;
 
-})
+	themeSwitch.addEventListener('click', toggleTheme);
 
-function prueba() {
-	if (checkbox.checked == true) {
-		console.log('Prendido');
-	} else if (checkbox.checked == false) {
-		console.log('Apagado');
+	function toggleTheme() {
+		const setTheme = themeSwitch.checked ? 'dark' : 'light';
+
+		root.setAttribute('class', setTheme);
 	}
-}
+});
