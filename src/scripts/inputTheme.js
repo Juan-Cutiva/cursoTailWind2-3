6c837b42f30aeca9d6f8a30e2f5b818beee5ee97
Juan David Cutiva <69434726+Juan-Cutiva@users.noreply.github.com>
2024-09-26 12:41:16 -0500

    dark mode dynamic

--- src/scripts/inputTheme.js
+++ src/scripts/inputTheme.js
@@ -3,6 +3,21 @@ const checkbox = document.getElementById('theme-switch__checkbox');
 
 themeSwitch.addEventListener('change', prueba);
 
+
+document.addEventListener('DOMContentLoaded', () => {
+	const themeSwitch = document.querySelector('#theme-switch__checkbox');
+	const root = document.documentElement
+
+	themeSwitch.addEventListener('click', toggleTheme)
+
+	function toggleTheme(){
+		const setTheme = themeSwitch.checked ? 'dark':'light'
+
+		root.setAttribute('class', setTheme)
+	}
+
+})
+
 function prueba() {
 	if (checkbox.checked == true) {
 		console.log('Prendido');
